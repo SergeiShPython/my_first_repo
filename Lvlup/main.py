d02579bc95b018e0dbde6f10cecabe8df6c48b60
SergeiShPython <Arhangelus@yandex.ru>
2023-03-03 21:06:32 +0300

    3

--- Lvlup/main.py
+++ Lvlup/main.py
@@ -2,14 +2,13 @@ x=float(input())
 y=float(input())
 if x == y:
     print("они равны")
-elif x<y:
-print(x)
-print(y)
-elif x>y:
-print(y)
-print(x)
-else:
-    print("они равны")
+elif x < y:
+    print(x)
+    print(y)
+elif x > y:
+    print(y)
+    print(x)
+
 
 
 
