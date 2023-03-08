676d514e07cf6ea46a70c307ee535b8bd8407935
SergeiShPython <Arhangelus@yandex.ru>
2023-03-08 14:26:13 +0300

    08032023

--- Lvlup/Homework/Homework1-2.py
+++ Lvlup/Homework/Homework1-2.py
@@ -1,11 +1,16 @@
-print("Введите список чисел:")
-list_of_inputs = input()
-list_of_inputs=[int(num) for num in list_of_inputs]
-x=0
-for i in list_of_inputs:
-    y=list_of_inputs[i+1]
-#    for y in list_of_inputs:
-    if i != y:
-       continue
-    else: print(i)
-
+list_of_input = input("Введите список чисел:")
+variable = []
+for i in list_of_input:
+    x = 0
+    for j in list_of_input:
+        if i == j:
+            x += 1
+    if x == 1:
+        variable.append(i)
+print(variable)
+# или так
+variable2 = []
+for u in list_of_input:
+    if list_of_input.count(u) == 1:
+        variable2.append(u)
+print(variable2)
