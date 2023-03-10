e77273cd2347afd609c06dad6c83a1e5d2c07854
SergeiShPython <Arhangelus@yandex.ru>
2023-03-10 19:26:43 +0300

    10032023

--- Lvlup/Homework/Homework2_2.py
+++ Lvlup/Homework/Homework2_2.py
@@ -1,6 +1,5 @@
 list_of_numbers =input()
 list_of_numbers.split(" ")
-list_of_numbers2 = []
-list_of_numbers2 = list_of_numbers
-print(list_of_numbers[::-1])
-print(len(list_of_numbers))
+print(list_of_numbers1[::-1])
+print(list_of_numbers1)
+print(len(list_of_numbers2))
