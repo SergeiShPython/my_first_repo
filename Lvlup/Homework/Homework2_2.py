80204decd944e5ab4a20efbedb54f095554a479f
SergeiShPython <Arhangelus@yandex.ru>
2023-03-10 22:12:15 +0300

    10032023

--- Lvlup/Homework/Homework2_2.py
+++ Lvlup/Homework/Homework2_2.py
@@ -1,4 +1,7 @@
 enter_string =input("Введите числа через пробел:")
-print(enter_string.split()[::-1])
-#print(list_of_numbers[::-1])
 print(enter_string[::-1])
+print(enter_string.split()[::-1])
+end_list = enter_string.split()
+print(end_list[::-1])
+
+#вывел разными способами
