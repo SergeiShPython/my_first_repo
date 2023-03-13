8c8d82f5cbcf7cf72226eaae669dab937a736c33
SergeiShPython <Arhangelus@yandex.ru>
2023-03-13 21:08:18 +0300

    13032023

--- Lvlup/Homework/Homework3_2v2recursy.py
+++ Lvlup/Homework/Homework3_2v2recursy.py
@@ -1,8 +1,7 @@
 contribution = int(input("Введите предполагаемый вклад: "))
 term = int(input("Введите срок в ГОДАХ!)): "))
 percent = float(input("Введите процентную ставку: "))
-result = float()
-def my_bank():
-    result = contribution * ((1+(percent/100))**term)
-    return result
-print(my_bank())
+def my_bank(term):
+    return 1 if term== 0 else (1+(percent/100))*my_bank(term-1)
+print(contribution*my_bank(term))
+
