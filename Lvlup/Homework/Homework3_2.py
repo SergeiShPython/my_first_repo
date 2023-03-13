ceea3be65ecea241ec1a3e06aa807bb29b922da2
SergeiShPython <Arhangelus@yandex.ru>
2023-03-13 18:26:08 +0300

    13032023

--- Lvlup/Homework/Homework3_2.py
+++ Lvlup/Homework/Homework3_2.py
@@ -1,6 +1,11 @@
+p = int(input("Введите предполагаемый вклад: "))
+t = int(input("Введите срок в ГОДАХ!)): "))
+i = float(input("Введите проценты: "))
+s = float()
 def my_bank():
-    start_contribution = int(input())
-    term = int(input())
-    size_of_contribution =start_contribution * term
-    print(size_of_contribution)
-my_bank()
+    s = p * ((1+(i/100))**t)
+    return s
+print(my_bank())
+
+
+
