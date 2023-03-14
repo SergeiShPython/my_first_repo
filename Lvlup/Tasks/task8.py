f84d5e659a9fec1e0ed7ed1e7b0633ef4a0ca349
SergeiShPython <Arhangelus@yandex.ru>
2023-03-14 20:06:14 +0300

    13032023

--- Lvlup/Tasks/task8.py
+++ Lvlup/Tasks/task8.py
@@ -1,6 +1,6 @@
 i = int(input())
 def year(i):
-   if i%4 == 0 and i%400 == 0 and i%100 != 0:
+   if (i%4 == 0 and i%400 == 0) or (i%4 == 0 and i%100 != 0):
        return "високосный"
    else: return "не високосный"
 print(year(i))
