7d68e9fffb27a5b4cb4fe534a31f079360e428ed
SergeiShPython <Arhangelus@yandex.ru>
2023-03-17 19:36:03 +0300

    17032023

--- Lvlup/Homework/Homework_3/Homework3_2v2recursy.py
+++ Lvlup/Homework/Homework_3/Homework3_2v2recursy.py
@@ -3,8 +3,7 @@ term = int(input("Введите срок в ГОДАХ!)): "))
 percent = float(input("Введите процентную ставку: "))
 def my_bank(term):
     return 1 if term== 0 else (1+(percent/100))*my_bank(term-1)
-my_bank(term)
+my_bank(term)10
 def final_contribution(contribution,my_bank):
     return contribution * my_bank
 print(f"Ваш вклад составит через {term} года(лет):", final_contribution(contribution,my_bank(term)), "денег")
-
