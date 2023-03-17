fe271b8e42e2f0a6b1f183bc1efff8a3b2edbcb8
SergeiShPython <Arhangelus@yandex.ru>
2023-03-17 18:50:35 +0300

    17032023

---
+++ Lvlup/Homework/Homework3_5.py
@@ -0,0 +1,10 @@
+
+list_of_numbers = [5, 6, 6, 8, 8, 31, 33, 44, 44, 999]
+def different_elements(list_of_numebrs, amount = 0):
+    for i in range(len(list_of_numebrs)):
+        if i == list_of_numbers[0] or list_of_numbers[i] != list_of_numebrs[i-1]:
+            amount = amount + 1
+
+    return (amount)
+
+print(different_elements(list_of_numbers))
