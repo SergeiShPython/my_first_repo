e320dd9b91173ec6d2df114c4fdb03bdbb1ccae1
SergeiShPython <Arhangelus@yandex.ru>
2023-03-21 15:01:13 +0300

    21032023

--- Lvlup/Homework/Homework_5/Homework5_1.py
+++ Lvlup/Homework/Homework_5/Homework5_1.py
@@ -1,5 +1,5 @@
 #1. Напишите программу, принимающую на вход слова через запятую и выводящую слова в алфавитном порядке. Используйте List comprehension.
-lst = input()
+lst = input().lower()
 sort_lst = sorted([i for i in lst.split(",")])
 print(sort_lst)
 
