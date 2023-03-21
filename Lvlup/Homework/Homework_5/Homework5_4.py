6328100160d8d3240f1ca893314e63073398463c
SergeiShPython <Arhangelus@yandex.ru>
2023-03-21 18:21:21 +0300

    21032023

--- Lvlup/Homework/Homework_5/Homework5_4.py
+++ Lvlup/Homework/Homework_5/Homework5_4.py
@@ -2,7 +2,7 @@
 import random
 lst = [random.randint(0,100 ) for i in range(10)]
 print(random.sample(lst, len(lst)))
-lst2 = random.sample([i for i in range(random.randint(1, 1000))], random.randint(1, 30))
+lst2 = random.sample([i for i in range(random.randint(1, 1000))], random.randint(0, 30))
 print(lst2)
 lst3 =[random.sample(lst, len(lst)) for i in range(len(lst))]
 print(lst3)
