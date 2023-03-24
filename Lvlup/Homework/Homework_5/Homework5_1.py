a016bb8c540135730d2abad29b6578e79a09b83b
SergeiShPython <Arhangelus@yandex.ru>
2023-03-24 19:07:47 +0300

    21032023

--- Lvlup/Homework/Homework_5/Homework5_1.py
+++ Lvlup/Homework/Homework_5/Homework5_1.py
@@ -2,6 +2,3 @@
 lst = input().lower()
 sort_lst = sorted([i for i in lst.split(",")])
 print(sort_lst)
-
-
-#print(list(map(lambda i : sorted(lst.split(",")),lst))) хотел через лямбду, она даже сортирует и выводит как нужно, но почему-то список выводится очень много  его копий
